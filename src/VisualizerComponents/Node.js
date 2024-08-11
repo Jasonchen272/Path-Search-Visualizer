990721ddcbf1814d65204dc0e38e3d91b7523fd4
Jason Chen <jchen.012004@gmail.com>
2024-08-11 17:39:19 -0500

    hover feature

--- src/VisualizerComponents/Node.js
+++ src/VisualizerComponents/Node.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import {React, useState} from 'react'
 import './Node.css'
 
 function Node({visited}) {
+    const [hasVisited, setHasVisited] = useState(visited)
     return (
-        <div className='path-node'>{visited ? "1" : "0"}</div>
+        <div className='path-node' onMouseOver={() => {setHasVisited(!hasVisited)}}>{hasVisited ? "1" : "0"}</div>
     )
 }
 
